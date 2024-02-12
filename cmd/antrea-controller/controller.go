a725816692302e4d6392a4b5e48354841a9254b3
Griffin <prakritimandal611@gmail.com>
2024-02-12 17:29:11 +0530

    chore: adding log entries and log files
    Signed-off-by: Griffin <prakritimandal611@gmail.com>

--- cmd/antrea-controller/controller.go
+++ cmd/antrea-controller/controller.go
@@ -122,6 +122,7 @@ var allowedPaths = []string{
 // run starts Antrea Controller with the given options and waits for termination signal.
 func run(o *Options) error {
 	klog.Infof("Starting Antrea Controller (version %s)", version.GetFullVersion())
+	klog.InfoS("The github id is: prakrit55", "and the pod is", env.GetPodName())
 	// Create K8s Clientset, Aggregator Clientset, CRD Clientset and SharedInformerFactory for the given config.
 	// Aggregator Clientset is used to update the CABundle of the APIServices backed by antrea-controller so that
 	// the aggregator can verify its serving certificate.
